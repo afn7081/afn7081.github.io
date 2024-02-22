97cca9cacd46d246caaededcbe0ada720a5b8713
afn7081 <khan.afnan1117@gmail.com>
2024-02-22 21:27:44 +0530

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/will-you-date",
+  base: "/src",
   plugins: [react()],build:{
     outDir: "build"
   }
